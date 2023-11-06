858cde88e7fe1f12aa2ac7e0ac65744ce27c389d
ajayb121 <ajaybrahma93@gmail.com>
2023-11-06 10:11:57 +0000

    update page component

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import ImageEditor from "@/app/components/ImageEditor";
 export default function Home() {
   return (
     <>
-      <header className="pt-12 text-center pb-5">
-        <h1 className="text-3xl font-bold">Image Editor</h1>
+      <header className="pt-12 text-center pb-7">
+        <h1 className="text-3xl font-bold text-slate-50">Image Editor</h1>
       </header>
-      <main className='max-w-xl mx-auto'>
+      <main className='max-w-5xl mx-auto'>
         <ImageEditor />
       </main>
     </>
